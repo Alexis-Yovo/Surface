26afcd8a3628be1bd4bc95387204bc886d6bc41b
Alexis-Yovo <74179398+Alexis-Yovo@users.noreply.github.com>
2024-01-29 15:06:10 +0100

    Add files via upload

---
+++ ex 3 la bagarre champs.py
@@ -0,0 +1,19 @@
+A=int(input("Surface du champ Arignon:"))
+E=int(input("Surface du champ Evaran:"))
+if (A<=E):
+    print ("La famille Evaran a un champ supérieur ou égal par rapport à la famille Arignon")
+else:
+    print("La famille Arignon a un champ supérieur ou égal par rapport à la famille Evaran")
+
+if (A+10<=E):
+    print("La famille Evaran a un champ trop grand") 
+else:
+
+    print("La famille Arignon a un champ trop petit")
+
+
+
+
+
+
+
